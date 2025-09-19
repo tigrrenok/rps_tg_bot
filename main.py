cd7bfc72e37678b8b88a2842939060aab98ae75c
Nadya <neu5ac@mail.ru>
2025-09-19 13:25:03 +0300

    init: create project structure with config

--- main.py
+++ main.py
@@ -4,38 +4,25 @@ import logging
 from aiogram import Bot, Dispatcher
 from config.config import Config, load_config
 from handlers import user, other
+from keyboards import keyboards
 
-# Импортируем роутеры
-# ...
-# Импортируем миддлвари
-# ...
-# Импортируем вспомогательные функции для создания нужных объектов
-# ...
-#from keyboards.main_menu import set_main_menu
-
-# Инициализируем логгер
 logger = logging.getLogger(__name__)
 
 
-# Функция конфигурирования и запуска бота
 async def main() -> None:
+    """Функция конфигурирования и запуска бота"""
 
-    # Загружаем конфиг в переменную config
     config: Config = load_config()
-    # Задаём базовую конфигурацию логирования
     logging.basicConfig(
         level=config.log.level,
         format=config.log.format,
     )
-    # Инициализируем бот и диспетчер
     bot = Bot(token=config.bot.token)
     dp = Dispatcher()
     dp.workflow_data.update({'token': config.bot.token})
 
-    # Регистриуем роутеры в диспетчере
     dp.include_router(user.router)
 
-    # Пропускаем накопившиеся апдейты и запускаем polling
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
